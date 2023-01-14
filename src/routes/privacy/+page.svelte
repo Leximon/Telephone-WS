cbcea6caf00c9a06f9d4c8e2a6f7b7177da77543
Leximon <lex305@web.de>
2023-01-15 00:11:49 +0100

    update privacy policy

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -14,8 +14,8 @@
 <h2>Information Collection and Use</h2>
 
 <p>Telephone does not collect or store any personal information from users. The only information we collect is related
-    to the voice channels that are being connected through our bot. This information is only used for the purpose of
-    connecting the voice channels and is not shared or used for any other purpose.</p>
+    to the settings of the guilds where the bot is active. This information is only used for the purpose of connecting
+    the voice channels and is not shared or used for any other purpose.</p>
 
 <h2>Voice Data</h2>
 
@@ -26,6 +26,12 @@
 
 <p>Telephone does not share any information with third parties. The information we collect is only used to connect the
     voice channels and is not shared or used for any other purpose.</p>
+
+<h2>Data Retention and Deletion</h2>
+
+<p>The data associated with your use of Telephone is retained on our servers for as long as you or your server continues
+    to use the bot. If you or the server "kicks" the bot, this data will be deleted from our servers. If you wish to
+    delete your data before that, please contact us.</p>
 
 <h2>Changes to this Privacy Policy</h2>
 
