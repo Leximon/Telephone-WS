460551bcc0f815e567fb9b4231e3f75bbbe172f5
Leximon <lex305@web.de>
2023-03-15 16:35:33 +0100

    pls 4 💀

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,12 +1,14 @@
-import { env } from '$env/dynamic/private';
-import type { DiscordLocale } from "$lib/types";
+import { DB_CONN_STRING } from '$env/static/private';
+import type { DiscordLocale } from '$lib/types';
 import { MongoClient, Db, Long } from "mongodb";
 
 export let db: Db
 await connectToDatabase()
 
 async function connectToDatabase() {
-    const client = new MongoClient(env.DB_CONN_STRING)
+    if (DB_CONN_STRING === undefined) // work around to prevent error during build
+        return
+    const client = new MongoClient(DB_CONN_STRING)
     await client.connect()
     db = client.db("telephone")
     console.log("Connected to database!")
